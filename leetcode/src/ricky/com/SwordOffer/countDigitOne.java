ea990962fbda7d1f8d51bb2c47f770923725094b
rickyzzzxu <98084369+rickyzzzxu@users.noreply.github.com>
2023-02-25 23:25:14 +0800

    Update countDigitOne.java

--- leetcode/src/ricky/com/SwordOffer/countDigitOne.java
+++ leetcode/src/ricky/com/SwordOffer/countDigitOne.java
@@ -7,25 +7,25 @@ public class countDigitOne {
         public int countDigitOne(int n) {
             long bit = 1;
             long cur = n / bit % 10;
-            long high = n / bit / 10;
             long low = n % bit;
+            long high = n / bit / 10;
             long res = 0;
-            while(bit <= n){
-                if(cur < 1){
-                    res = res + high * bit;
-                }else if(cur == 1){
-                    res = res + high * bit + low + 1;
-                }else{
-                    res = res + (high + 1) * bit;
+            while (bit <= n) {
+                if (cur > 1) {
+                    res += (high + 1) * bit;
+                } else if (cur == 1) {
+                    res += high * bit + low + 1;
+                } else {
+                    res += high * bit;
                 }
 
                 bit *= 10;
                 cur = n / bit % 10;
-                high = n / bit / 10;
                 low = n % bit;
+                high = n / bit / 10;
             }
 
-            return (int)res;
+            return (int) res;
         }
     }
 }
