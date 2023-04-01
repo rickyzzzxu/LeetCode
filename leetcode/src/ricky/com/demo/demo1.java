ed1293c6ba2361d073d9cc9c36bf82156691291e
rickyzzzxu <rickyzzzz@163.com>
2023-04-01 19:36:30 +0800

    mirrorTree

--- leetcode/src/ricky/com/demo/demo1.java
+++ leetcode/src/ricky/com/demo/demo1.java
@@ -5,7 +5,11 @@ package ricky.com.demo;
  * @Date 2023/2/27 19:49
  */
 public class demo1 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        demo2 demo2 = new demo2();
+        int[] hei = {2, 1, 5, 6, 2, 3};
+        System.out.println(demo2.numTrees(3));
     }
+
 }
