092131a80a355ff21d76cdce91f3bc841eb3e338
rickyzzzxu <rickyzzzz@163.com>
2023-04-09 18:00:56 +0800

    productExceptSelf

---
+++ leetcode/src/ricky/com/Hot100/productExceptSelf.java
@@ -0,0 +1,34 @@
+package ricky.com.Hot100;
+
+/**
+ * @Author xdg
+ * @Date 2023/4/9 18:00
+ */
+public class productExceptSelf {
+    /*238. 除自身以外数组的乘积
+    给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+    题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+    请不要使用除法，且在 O(n) 时间复杂度内完成此题。*/
+    class Solution {
+        public int[] productExceptSelf(int[] nums) {
+            int n = nums.length;
+            int[] left = new int[n];
+            int[] right = new int[n];
+            left[0] = 1;
+            right[n - 1] = 1;
+            for (int i = 1; i < n; i++) {
+                left[i] = left[i - 1] * nums[i - 1];
+            }
+
+            for (int i = n - 2; i >= 0; i--) {
+                right[i] = right[i + 1] * nums[i + 1];
+            }
+
+            for (int i = 0; i < n; i++) {
+                nums[i] = left[i] * right[i];
+            }
+
+            return nums;
+        }
+    }
+}
