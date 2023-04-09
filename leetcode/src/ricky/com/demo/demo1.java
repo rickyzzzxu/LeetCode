ff898d8de4680ef57f827596c48be7920a2b96fd
rickyzzzxu <rickyzzzz@163.com>
2023-04-09 17:31:07 +0800

    maximalSquare

--- leetcode/src/ricky/com/demo/demo1.java
+++ leetcode/src/ricky/com/demo/demo1.java
@@ -1,8 +1,8 @@
 package ricky.com.demo;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @Author xdg
@@ -11,25 +11,11 @@ import java.util.List;
 public class demo1 {
 
     public static void main(String[] args) {
-        int[] numbers = {2,2,2,0,1};
-        int l = 0;
-        int r = numbers.length - 1;
-        while (l <= r) {
-            int mid = (r - l) / 2 + l;
-            if (numbers[l] <= numbers[r]) {
-                System.out.println(numbers[l]);
-            }
 
-            if (numbers[mid] > numbers[l]) {
-                l = mid + 1;
-            } else if (numbers[mid] < numbers[l]) {
-                r = mid;
-            } else {
-                l++;
-            }
-        }
-
-        System.out.println(numbers[l]);
+        char a = '1';
+        int b = a - '0';
+        System.out.println(a);
+        System.out.println(b);
     }
 
 }
