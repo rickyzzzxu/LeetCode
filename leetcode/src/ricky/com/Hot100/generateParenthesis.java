58c8c69bbb28c387d9e2cdf1773b7bbc69508e81
rickyzzzxu <rickyzzzz@163.com>
2023-05-24 22:58:07 +0800

    update

--- leetcode/src/ricky/com/Hot100/generateParenthesis.java
+++ leetcode/src/ricky/com/Hot100/generateParenthesis.java
@@ -12,30 +12,32 @@ public class generateParenthesis {
     数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。*/
     class Solution {
         List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int n;
 
         public List<String> generateParenthesis(int n) {
-            StringBuilder sb = new StringBuilder();
-            dfs(sb, 0, 0, n);
+            if (n == 0) {
+                return res;
+            }
+
+            this.n = n;
+            dfs(0, 0, sb);
             return res;
         }
 
-        private void dfs(StringBuilder sb, int l, int r, int n) {
-            //当右括号多余左括号时退出
+        private void dfs(int l, int r, StringBuilder sb) {
             if (l > n || r > n || r > l) {
                 return;
             }
-
             if (sb.length() == n * 2) {
                 res.add(sb.toString());
+                return;
             }
-
-            //先拼个左括号
-            sb.append('(');
-            dfs(sb, l + 1, r, n);
+            sb.append("(");
+            dfs(l + 1, r, sb);
             sb.deleteCharAt(sb.length() - 1);
-            //然后拼个右括号
-            sb.append(')');
-            dfs(sb, l, r + 1, n);
+            sb.append(")");
+            dfs(l, r + 1, sb);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
